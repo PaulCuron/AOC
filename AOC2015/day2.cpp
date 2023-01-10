7c62cbddbbea3aae2f466875ca27559e6e0e43f8
PaulCuron <79709106+PaulCuron@users.noreply.github.com>
2023-01-10 15:03:31 +0100

    mise en forme

--- AOC2015/day2.cpp
+++ AOC2015/day2.cpp
@@ -11,12 +11,13 @@ int main(){
 	std::string line;
 	std::size_t pos = 0;
 	int size_tot = 0;
-	int l_ribbon = 0;
+	int length_tot = 0;
 	int s; // each packet face surface
 	int S; // each packet total surface
 	int l; // each packe ribbon length
 	std::string sdim;
 	int dim; // each dimension
+	
 	if( file.is_open() ) {
 		
 		while (std::getline(file, line)) {
@@ -42,21 +43,22 @@ int main(){
 			S = 0;
 			int lmin = 2000000;
 			int smin = 2000000;
+			
 			for (int i = 0; i < dims.size()-1; i++) {
 				for (int j = i+1; j < dims.size(); j++){
 					s = dims[i]*dims[j]; // face surf
 					l = dims[i] + dims[j]; // face perimeter
 					smin = std::min(s,smin);
 					lmin = std::min(l,lmin);
-			 		S = S + 2*s;
+			 		S += 2*s;
 				}
-			
+				int V = dims[0]*dims[1]*dims[2]; // volume of the packet
 			}
-			l_ribbon += 2*lmin + dims[0]*dims[1]*dims[2];
+			length_tot += 2*lmin + V;
 			size_tot += S + smin;
 		}
 		std::cout << "Part 1: " << size_tot << "\n";	
-		std::cout << "Part 2: " << l_ribbon << "\n";
+		std::cout << "Part 2: " << length_tot << "\n";
 	}
 	return 0;
 }
